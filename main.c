600feb75ca131df8655afa897c8446d50b53810e
kimulu <kimulumichael@gmail.com>
2023-12-13 00:24:51 -0800

    new commit

--- main.c
+++ main.c
@@ -1,57 +1,76 @@
 #include "main.h"
 
 /**
-* main - main entry point
-* @argc: argument count
-* @argv: command line arguments
-* Return: 0
-*/
+ * print_prompt - Print shell prompt.
+ */
+void print_prompt(void)
+{
+    if (isatty(STDIN_FILENO))
+        printf("Shell-Prompt> ");
+}
 
+/**
+ * handle_exit - Handle the exit command.
+ * @line: Input line.
+ */
+bool handle_exit(char *line)
+{
+    if (strcmp(line, "exit") == 0)
+    {
+        free(line);
+        return true;
+    }
+    return false;
+}
+
+/**
+ * process_command - Process the entered command.
+ * @line: Input line.
+ * @counter: Command counter.
+ * @argv: Command line arguments.
+ */
+void process_command(char *line, size_t counter, char *argv[])
+{
+    char **tokens;
+
+    if (*line == '\0' || !(tokens = tokenize_command(line, " ")))
+        return;
+
+    if (access(tokens[0], X_OK) == -1 && (find_path(&tokens[0]), access(tokens[0], X_OK) == -1))
+        fprintf(stderr, "%s: %lu: %s: not found\n", argv[0], counter, tokens[0]);
+    else
+        execute_command(tokens);
+
+    free(line);
+    deallocate_tokens(tokens);
+}
+
+/**
+ * main - Main entry point.
+ * @argc: Argument count.
+ * @argv: Command line arguments.
+ * Return: 0.
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-char *pointer_to_line = NULL, **command_tokens = NULL;
-size_t line_size = 0, counter = 0;
-ssize_t char_read;
+    char *line = NULL;
+    size_t size = 0, counter = 0;
+    ssize_t read_chars;
 
-while (true)
-{
-if (isatty(STDIN_FILENO))
-{
-show_prompt();
-}
-fflush(stdout);
-char_read = read_input(&pointer_to_line, &line_size);
-++counter;
-getline_errors(char_read, pointer_to_line);
-remove_newline(pointer_to_line, char_read);
-if (*pointer_to_line == '\0')
-continue;
-if (strcmp(pointer_to_line, "exit") == 0)
-{
-free(pointer_to_line);
-exit(EXIT_SUCCESS);
-}
-command_tokens = tokenize_command(pointer_to_line, " ");
-if (command_tokens == NULL)
-continue;
+    while (true)
+    {
+        print_prompt();
+        fflush(stdout);
 
-if (access(command_tokens[0], X_OK) == -1)
-{
-find_path(&command_tokens[0]);
-if (command_tokens != NULL && access(command_tokens[0], X_OK) == -1)
-{
-fprintf(stderr, "%s: %lu: %s: not found\n", argv[0],
-counter, command_tokens[0]);
-deallocate_tokens(command_tokens);
-continue;
-}
-}
-execute_command(command_tokens);
-free(pointer_to_line);
-deallocate_tokens(command_tokens);
-pointer_to_line = NULL;
-command_tokens = NULL;
-}
-return (0);
+        read_chars = read_input(&line, &size);
+        getline_errors(read_chars, line);
+        remove_newline(line, read_chars);
+
+        if (handle_exit(line))
+            return 0;
+
+        process_command(line, ++counter, argv);
+        line = NULL;
+    }
 }
 
